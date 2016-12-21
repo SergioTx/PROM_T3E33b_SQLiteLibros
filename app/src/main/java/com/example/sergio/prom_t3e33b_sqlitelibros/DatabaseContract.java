4e61af654de9cc6cd151a711ef2d9e39ce8260ac
SergioTx <txitxo37@gmail.com>
2016-12-21 09:53:49 +0100

    cambios nombres y añadido TODO

--- app/src/main/java/com/example/sergio/prom_t3e33b_sqlitelibros/DatabaseContract.java
+++ app/src/main/java/com/example/sergio/prom_t3e33b_sqlitelibros/DatabaseContract.java
@@ -12,9 +12,11 @@ public final class DatabaseContract {
 
     public static final  int    DATABASE_VERSION   = 1;
     public static final  String DATABASE_NAME      = "database.db";
-    private static final String TEXT_TYPE          = " TEXT";
-    private static final String INTEGER_TYPE          = " INTEGER";
-    private static final String COMMA_SEP          = ",";
+
+    private static final String PRIMARY_KEY         = " PRIMARY KEY";
+    private static final String TEXT_TYPE           = " TEXT";
+    private static final String INTEGER_TYPE        = " INTEGER";
+    private static final String COMMA_SEP           = ",";
 
     // Para prevenir la instanciación, constructor privado y vacío
     private DatabaseContract() {}
@@ -24,22 +26,24 @@ public final class DatabaseContract {
      * Implementa BaseColumns: https://developer.android.com/training/basics/data-storage/databases.html#DbHelper
      */
     public static abstract class Libros implements BaseColumns {
-        public static final String TABLE_NAME       = "libros";
-        public static final String COLUMN_NAME_COL1 = "isbn";
-        public static final String COLUMN_NAME_COL2 = "titulo";
-        public static final String COLUMN_NAME_COL3 = "autor";
-        public static final String COLUMN_NAME_COL4 = "anio";
+        public static final String TABLE_NAME           = "libros";
+        public static final String COLUMN_NAME_ISBN     = "isbn";
+        public static final String COLUMN_NAME_TITULO   = "titulo";
+        public static final String COLUMN_NAME_AUTOR    = "autor";
+        public static final String COLUMN_NAME_ANIO     = "anio";
 
-        /**
-         * http://stackoverflow.com/questions/3192064/about-id-field-in-android-sqlite
+        /*
+         * campo _ID: http://stackoverflow.com/questions/3192064/about-id-field-in-android-sqlite
+         * El campo _ID puede ponerse como alias de ISBN y vale para un CursorAdapter
+         * TODO Cursor adapter con listview - https://github.com/codepath/android_guides/wiki/Populating-a-ListView-with-a-CursorAdapter
          */
         public static final String CREATE_TABLE = "CREATE TABLE " +
                 TABLE_NAME + " (" +
-                _ID + INTEGER_TYPE + " PRIMARY KEY" + COMMA_SEP +
-                COLUMN_NAME_COL1 + TEXT_TYPE + COMMA_SEP +
-                COLUMN_NAME_COL2 + TEXT_TYPE + COMMA_SEP +
-                COLUMN_NAME_COL3 + TEXT_TYPE + COMMA_SEP +
-                COLUMN_NAME_COL4 + INTEGER_TYPE + " )";
+                    //_ID + INTEGER_TYPE + PRIMARY_KEY + COMMA_SEP +
+                    COLUMN_NAME_ISBN + INTEGER_TYPE + PRIMARY_KEY + COMMA_SEP +
+                    COLUMN_NAME_TITULO + TEXT_TYPE + COMMA_SEP +
+                    COLUMN_NAME_AUTOR + TEXT_TYPE + COMMA_SEP +
+                    COLUMN_NAME_ANIO + INTEGER_TYPE + " )";
 
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
     }
